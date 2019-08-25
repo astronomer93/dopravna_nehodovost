d19f7fce756bb4a1cf43e8d02073daa46e2e4637
ástronomer93 <exospace.bio@gmail.com>
2019-08-25 17:23:02 +0200

    riesenie symbolickych linkov po zmazanych suboroch

---
+++ zmazanieSymbolickychLinkov.sh
@@ -0,0 +1,8 @@
+#find /home/astronomer93/dopravna_nehodovost -maxdepth '*~' -name '$retazec' -exec rm -rf {} \;
+#find /home/astronomer93/dopravna_nehodovost -maxdepth 10 -name '.RData' -exec rm -rf {} \;
+#find /home/astronomer93/dopravna_nehodovost -maxdepth 10 -name '.Rhistory' -exec rm -rf {} \;
+
+#funguje len v prikazovom riadku 
+
+
+
